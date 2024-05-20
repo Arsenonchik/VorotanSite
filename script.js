3468c90a9bf3f5e4ffc26fe33a34227fb67437a3
Arsen <arsen.grigoryan.29.2001@gmail.com>
2024-05-20 19:10:34 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function() {
+    $("#submenu").slideToggle(500);
+    $("#item").click(function() {
+        $("#submenu").slideToggle(500);
+    });
+}); 
+$(function() {
+    $("#submenu2").slideToggle(500);
+    $("#item2").click(function() {
+        $("#submenu2").slideToggle(500);
+    });
+}); 
